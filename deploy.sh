507f1ca712b977aab15be73ff598e6ddb0a8c305
Victor Viale <victor.viale@besedo.com>
2019-06-10 12:00:36 +0200

    Update

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-if [[ "$1" == "-y" ]]
-then
-  deploy
-fi
-
-read -p "Are you sure? Rewrite will occur" -n 1 -r
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-  deploy
-fi
-
 deploy () {
   cp .gitconfig ~/.gitconfig
   cp .githelpers ~/.githelpers
@@ -19,3 +8,5 @@ deploy () {
   cp .psqlrc ~/.psqlrc
   mkdir -p .config/nvim && cp .config/nvim/init.vim ~/.config/nvim/init.vim
 }
+
+deploy
