cfb8226bdbbebf80220154de85cc2d7838921b9b
Victor Viale <victor.viale@besedo.com>
2024-12-02 22:35:41 +0100

    chezmoi!

--- obtain-local.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp ~/.gitconfig .gitconfig
-cp ~/.githelpers .githelpers
-cp ~/.zshrc .zshrc
-cp ~/.functions .functions
-mkdir -p .config/nvim && cp ~/.config/nvim/init.vim .config/nvim/init.vim
-cp ~/.psqlrc .psqlrc
-
---
+++ private_dot_config/nvim/lua/plugins/core.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+  {
+    "folke/twilight.nvim",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
+  -- {
+  --   "hiphish/rainbow-delimiters.nvim",
+  --   opts = {},
+  -- },
+}
